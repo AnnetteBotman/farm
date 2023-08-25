e1d9179afe3f0582c95cb4d24b22b81c4aa91a46
Annette <a.botman@casema.nl>
2023-08-25 19:36:33 +0200

    first commit

---
+++ test_main.py
@@ -0,0 +1,20 @@
+import pytest
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_index(client):
+    response = client.get("/cow")
+    assert response.status_code == 200
+    
+
+
+   
+
+
